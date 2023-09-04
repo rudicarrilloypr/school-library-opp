875cc073cfa52a8e3a6a1d2407c92c622ced9c83
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-09-04 16:47:00 -0600

    Fix linters

--- decorator.rb
+++ decorator.rb
@@ -1,6 +1,7 @@
 # This class acts as a base decorator for Nameable objects. It encapsulates the Nameable object
 class Decorator < Nameable
   def initialize(nameable)
+    super()
     @nameable = nameable
   end
 
