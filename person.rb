4a57311aeebbac68433a43b672119a831c70ea62
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-09-04 15:41:59 -0600

    Fix linters

--- person.rb
+++ person.rb
@@ -1,22 +1,22 @@
+# This class represents a Person with attributes like id, name, and age.
 class Person
-    attr_reader :id, :name, :age
-    attr_writer :name, :age
-  
-    def initialize(age, name = "Unknown", parent_permission = true)
-        @id = rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-      end
-  
-    def can_use_services?
-      of_age? || @parent_permission
-    end
-  
-    private
-  
-    def of_age?
-      @age >= 18
-    end
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
-  
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
--- student.rb
+++ student.rb
@@ -1,11 +1,11 @@
 require_relative 'person'
-
+# This class represents a Person with attributes like id, name, and age.
 class Student < Person
-    def initialize(age, classroom, name = "Unknown", parent_permission = true)
-        super(age, name, parent_permission)
-        @classroom = classroom
-    end
-     
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
   def play_hooky
     "¯\(ツ)/¯"
   end
