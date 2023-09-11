93a9db3a78ab3e09ce3fbf18ed0d996711a560b5
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-09-11 11:17:59 -0600

    Refacotize App.rb

--- app.rb
+++ app.rb
@@ -8,6 +8,9 @@ require_relative 'trimmer_decorator'
 require_relative 'classroom'
 require_relative 'bookclass'
 require_relative 'rental'
+require_relative 'book_factory'
+require_relative 'rental_factory'
+require_relative 'person_factory'
 
 # This class represents a Teacher with attributes like id, name, and age.
 class App
@@ -26,69 +29,22 @@ class App
   end
 
   def create_person
-    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-    choice = gets.chomp.to_i
-
-    print 'Age: '
-    age = gets.chomp.to_i
-
-    print 'Name: '
-    name = gets.chomp
-
-    if choice == 1
-      print 'Has parent permission? [Y/N]: '
-      parent_permission = gets.chomp.downcase == 'y'
-
-      classroom = Classroom.new('10th Grade') # for simplicity, assume all students are in 10th Grade
-      student = Student.new(age, classroom, name, parent_permission: parent_permission)
-      @people << student
-
-      puts 'Student created successfully'
-    elsif choice == 2
-      print 'Specialization: '
-      specialization = gets.chomp
-
-      teacher = Teacher.new(age, specialization, name)
-      @people << teacher
-
-      puts 'Teacher created successfully'
-    else
-      puts 'Invalid option'
-    end
+    person = PersonFactory.create_person
+    return unless person
+    
+    @people << person
+    puts "#{person.class} created successfully"
   end
-
+  
   def create_book
-    print 'Title: '
-    title = gets.chomp
-
-    print 'Author: '
-    author = gets.chomp
-
-    book = Book.new(title, author)
+    book = BookFactory.create_book
     @books << book
-
     puts 'Book created successfully'
   end
-
+  
   def create_rental
-    puts 'Select a book from the following list by number'
-    @books.each_with_index { |book, index| puts "#{index}) Title: \"#{book.title}\", Author: \"#{book.author}\"" }
-
-    book_index = gets.chomp.to_i
-
-    puts 'Select a person from the following list by number'
-    @people.each_with_index do |person, index|
-      puts "#{index}) [#{person.class}] Name: \"#{person.name}\", ID: #{person.id}, Age: #{person.age}"
-    end
-
-    person_index = gets.chomp.to_i
-
-    print 'Date: '
-    date = gets.chomp
-
-    rental = Rental.new(date, @books[book_index], @people[person_index])
+    rental = RentalFactory.create_rental(@books, @people)
     @rentals << rental
-
     puts 'Rental created successfully'
   end
 
---
+++ book_factory.rb
@@ -0,0 +1,12 @@
+class BookFactory
+    def self.create_book
+      print 'Title: '
+      title = gets.chomp
+  
+      print 'Author: '
+      author = gets.chomp
+  
+      Book.new(title, author)
+    end
+  end
+  
